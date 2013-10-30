ee76e58f08347727de6ff89d186f62cf2b83eb91
Satoru Yasuda <saadtuosrauy@live.jp>
2013-10-30 19:12:16 +0900

    first commit to "mk_sources.c"

--- hardware/fmul.vhd
+++ hardware/fmul.vhd
@@ -4,45 +4,80 @@ use IEEE.std_logic_unsigned.all;
 
 
 entity FMUL is
-port (A   : in std_logic_vector(31 downto 0);
-B   : in std_logic_vector(31 downto 0);
-clk : in std_logic;
-R   :out std_logic_vector(31 downto 0));
+	port (A   : in std_logic_vector(31 downto 0);
+	      B   : in std_logic_vector(31 downto 0);
+	      clk : in std_logic;
+	      R   :out std_logic_vector(31 downto 0));
 end FMUL;
 
 architecture fmul32 of FMUL is
 
---SIGNALS
-signal state	:integer range 0 to 10 :=0;
-signal fraction: std_logic_vector(8 downto 0);
-signal sign : std_logic;
-signal exponent: std_logic_vector(26 downto 0);
-signal stage : std_logic_vector(47 downto 0);
+	--SIGNALS
+	signal state	:std_logic;
+	signal fraction: std_logic_vector(8 downto 0);
+	signal sign : std_logic;
+	signal exponent: std_logic_vector(26 downto 0);
+	signal stage : std_logic_vector(47 downto 0);
+	signal result : std_logic_vector(31 downto 0);
 
+begin
+	process(A,B)
 	begin
-statemachine:process(A,B)
-	begin
-	fraction<=A(8 downto 1)+B(8 downto 1);
-	if fraction < 127 then
-	exponent<="000000000000000000000000000";
-	fraction<="00000000";
-	sign    <= ( A(0) xor B(0) );
-	state   <=0;
-	elsif fraction > 381 then
-	exponent<="000000000000000000000000000";
-	fraction<="00000000";
-	sign    <= ( A(0) xor B(0) );
-	state   <=1;
-	else
-	fraction<=fraction-127;
-	state   <=2;
-	end if;
-	stage <= ("0"&A(22 downto 0)) * ("0"&B(22 downto 0));
+		fraction<=("0"&A(8 downto 1))+("0"&B(8 downto 1));
+		stage <= ("0"&A(22 downto 0)) * ("0"&B(22 downto 0));
+		sign    <= ( A(0) xor B(0) );
 	end process;
 
 
+	process(stage)
+	begin
+		if fraction < 127 then
+			exponent<="000000000000000000000000000";
+			fraction<="00000000";
+			stage   <=x"000000000000";
+		elsif fraction > 381 then
+			exponent<="000000000000000000000000000";
+			fraction<="00000000";
+			stage   <=x"000000000000";
+		else
+			fraction<=fraction-127;
+			if (stage(21 downto 0) > 0) then exponent<=stage(47 downto 22)&"1";
+			else exponent<=stage(47 downto 22)&"0";
+			end if;
+		end if;
+	end process;
 
+	process(exponent)
+	begin
+		exponent<=(exponent(26 downto 2)&"00") + (x"000000"&(exponent(1) and (exponent(2) or exponent(0)))&"00" );
+		if (state='0') then state<='1';
+		end if;
+	end process;
 
+	process(state)
+	begin
+				--case (state) is
+				--when 1 => result <= sign&"0000000000000000000000000000000";
+				--when 3 => result <= sign&"1111111100000000000000000000000";
+				--when 2 => 
+		if (exponent(26)='1') then
+			fraction<=fraction+1;
+			exponent<=exponent(25 downto 0)&"0";
+		elsif (exponent(26 downto 1)="01111111111111111111111111") then
+			fraction<=fraction+1;
+			exponent<="000000000000000000000000000";
+		end if;
+		result<=sign&fraction(7 downto 0)&exponent(26 downto 4);
+				--end case;
+		state<='0';
+	end process;
 
-
+	process (result)
+	begin
+		if    fraction=0   then R<= sign&"0000000000000000000000000000000";
+		elsif fraction=255 then R<= sign&"1111111100000000000000000000000";
+		else R<=result;
+		end if;
+	end process;
+end;
 
--- hardware/fmul_tb.vhd
+++ hardware/fmul_tb.vhd
@@ -1,51 +1,78 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.std_logic_unsigned.all;
+USE ieee.std_logic_textio.all;
+LIBRARY STD;
+use std.textio.all;
 
 ENTITY fmul_tb IS
-END fmul_tb;
+	END fmul_tb;
 
 ARCHITECTURE FMUL_TESTBENCH OF fmul_tb IS 
   -- Component Declaration for the Unit Under Test (UUT)
-  COMPONENT FMUL
-    PORT(
-      A            : in  std_logic_vector(31 downto 0);
-      B            : in  std_logic_vector(31 downto 0);
-      clk          : in  std_logic;
-      R            : out std_logic_vector(31 downto 0) 
-      );
-  END COMPONENT;
+	COMPONENT FMUL
+		PORT(
+			    A            : in  std_logic_vector(31 downto 0);
+			    B            : in  std_logic_vector(31 downto 0);
+			    clk          : in  std_logic;
+			    R            : out std_logic_vector(31 downto 0) 
+		    );
+	END COMPONENT;
 
   --Inputs
-    signal iA  : std_logic_vector(31 downto 0) := (others => '0');
-    signal iB  : std_logic_vector(31 downto 0) := (others => '0');
+	signal iA  : std_logic_vector(31 downto 0) := (others => '0');
+	signal iB  : std_logic_vector(31 downto 0) := (others => '0');
 
   --Outputs
-    signal r : std_logic_vector(31 downto 0) := (others => '0');
+	signal r : std_logic_vector(31 downto 0) := (others => '0');
 
   --Signals
-    signal addr: std_logic_vector(7 downto 0) := "00000000";
-    signal simclk: std_logic := '0';
-BEGIN
-  uut: SLICE_ADDER PORT MAP (
-    clk => simclk,
-    i => i,
-    o => o
-    );
-  
- this_loops_forever: process(simclk)
-  begin
-    if rising_edge(simclk) then
-      i<=addr;
-      addr<=addr+1;
-    end if;
-  end process;
+	signal state: integer range 0 to 1 :=0;
+	signal simclk: std_logic := '0';
 
-  clockgen: process
-  begin
-    simclk<='0';
-    wait for 5 ns;
-    simclk<='1';
-    wait for 5 ns;
-  end process;
+	file A_LIST: text open read_mode is "alist.txt";
+	file B_LIST: text open read_mode is "blist.txt";
+	file RESULT: text open write_mode is "results.txt";
+
+
+BEGIN
+	uut: FMUL PORT MAP (
+				   A => iA,
+				   B => iB,
+				   clk=>simclk,
+				   R => r
+			   );
+
+	process(simclk)
+		variable li_A: line;
+		variable li_B: line;
+		variable vA: std_logic_vector(31 downto 0);
+		variable vB: std_logic_vector(31 downto 0);
+	begin
+		if rising_edge(simclk) then
+			case (state) is
+				when 0 =>
+					readline(A_LIST,li_A);
+					hread(li_A,vA);
+					iA<=vA;
+					readline(B_LIST,li_B);
+					hread(li_B,vB);
+					iB<=vB;
+					state<=1;
+				when 1 =>
+					hwrite(li_A,r,right,40);
+					writeline(RESULT,li_A);
+					state<=0;
+
+			end case;
+		end if;
+	end process;
+
+	clockgen: process
+	begin
+		simclk<='0';
+		wait for 5 ns;
+		simclk<='1';
+		wait for 5 ns;
+	end process;
 END;
